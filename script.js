1d54e983f184f3857c79b4bbc97d86ac4aa30029
Eric-Gona <eric.gona254@gmail.com>
2021-11-24 13:05:31 +0300

    changes js

--- script.js
+++ script.js
@@ -55,10 +55,3 @@ $(document).ready(function() {
   });
 });
 
-function resetFields() {
-    $("input#new-first-name").val("");
-    $("input#new-last-name").val("");
-    $("input.new-street").val("");
-    $("input.new-city").val("");
-    $("input.new-county").val("");
-}
